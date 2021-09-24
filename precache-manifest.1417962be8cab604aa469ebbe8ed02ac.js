59dd62d1cba324f15c14df0be2d0e4166d83df9d
HuyVo2112 <huyvolqd2112@gmail.com>
2021-09-24 09:36:14 -0400

    Updates

---
+++ precache-manifest.1417962be8cab604aa469ebbe8ed02ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbb10a1a26ecfee2396a754d100d3eb",
+    "url": "//huyvo2112.github.io/Burger-Builder/index.html"
+  },
+  {
+    "revision": "e4170eb1425832242c7f",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/css/3.2f8f16fd.chunk.css"
+  },
+  {
+    "revision": "10c032a96e46024d6b7f",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/css/main.3bb3cd90.chunk.css"
+  },
+  {
+    "revision": "c1d1c6e977a42fc83c2c",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/js/2.a0aecf5b.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/js/2.a0aecf5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4170eb1425832242c7f",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/js/3.d5f684d6.chunk.js"
+  },
+  {
+    "revision": "10c032a96e46024d6b7f",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/js/main.1a428e34.chunk.js"
+  },
+  {
+    "revision": "c99deebbbeb7218db7b5",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/js/runtime-main.6681a548.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "//huyvo2112.github.io/Burger-Builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
